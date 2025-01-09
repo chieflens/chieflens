28c0b6c3470a579d1e720bcb620d8f8262db0482
chieflens <chieflens@gmail.com>
2025-01-09 18:26:45 +0800

    2025010918264464

--- script1.js
+++ script1.js
@@ -30,7 +30,7 @@
                 }
             })
         };
-        handle({ currPath: `C:\\xxx\\2025010917013008` });
+        handle({ currPath: `C:\\xxx\\2025010918264464` });
         handle({ currPath: `C:\\xxx\\20250101` });
     }
 })();
